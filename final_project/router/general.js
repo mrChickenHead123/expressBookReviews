a39d678bcb296f39494df9dc7f7ada36c09b7181
mrChickenHead123 <226981646+mrChickenHead123@users.noreply.github.com>
2025-10-17 20:00:20 -0400

    task 1 & 2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,18 +13,24 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbnNumber = req.params.isbn;
+    let filteredISBNnumber = books.filter((book) => {
+        return book[isbnNumber] === isbnNumber
+    })
+    res.send(filteredISBNnumber)
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
